c59fca2f5117163f765deaf9a80f5ad3fad8c7f9
R00tl33t <reatin@yandex.ru>
2025-03-31 23:07:20 +0300

    Upgrade Readme, Makefile and Differ.java

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/R00tl33t/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/R00tl33t/java-project-71/actions)
+[Video of testing Differ. Step 6](https://asciinema.org/a/n5Qiqezo2cTjHoBFID9bw1OxO)
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,15 +4,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.HashSet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Differ {
-    public static String generate(String filePath1, String filePath2, String format)
-        throws Exception {
+    public static String generate(String filePath1, String filePath2, String format) throws Exception {
         Map<String, Object> data1 = parseFile(filePath1);
         Map<String, Object> data2 = parseFile(filePath2);
-        return String.format("File1: %s\nFile2: %s\nFormat: %s",
-            data1.toString(), data2.toString(), format);
+
+        return buildDiff(data1, data2);
     }
 
     private static Map<String, Object> parseFile(String filePath) throws Exception {
@@ -22,10 +24,36 @@ public class Differ {
         }
         String content = Files.readString(path);
         ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(content, Map.class);
-        } catch (Exception e) {
-            throw new Exception("Failed to parse JSON from file '" + path + "'", e);
+        return mapper.readValue(content, Map.class);
+    }
+
+    private static String buildDiff(Map<String, Object> data1, Map<String, Object> data2) {
+        Set<String> allKeys = new HashSet<>();
+        allKeys.addAll(data1.keySet());
+        allKeys.addAll(data2.keySet());
+
+        TreeMap<String, String> diff = new TreeMap<>();
+
+        for (String key : allKeys) {
+            Object value1 = data1.get(key);
+            Object value2 = data2.get(key);
+
+            if (!data1.containsKey(key)) {
+                diff.put(key, "+ " + key + ": " + value2);
+            } else if (!data2.containsKey(key)) {
+                diff.put(key, "- " + key + ": " + value1);
+            } else if (value1.equals(value2)) {
+                diff.put(key, "  " + key + ": " + value1);
+            } else {
+                diff.put(key, "- " + key + ": " + value1 + "\n" +
+                    "+ " + key + ": " + value2);
+            }
         }
+        StringBuilder result = new StringBuilder("{\n");
+        for (Map.Entry<String, String> entry : diff.entrySet()) {
+            result.append("  ").append(entry.getValue().replace("\n", "\n  ")).append("\n");
+        }
+        result.append("}");
+        return result.toString();
     }
 }
